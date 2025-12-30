737672994de0132dc2b33df81e79cde88264c20c
diredocks <26994007+diredocks@users.noreply.github.com>
2025-12-31 04:47:36 +0800

    pref(background): set openerTabId when create tab

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -2,8 +2,8 @@ import { Context } from './models/context';
 import { onMessage } from './utils/messaging';
 
 export default defineBackground(() => {
-  onMessage('Search', m => handleSearch(m.data));
-  onMessage('Open', m => handleOpen(m.data));
+  onMessage('Search', m => handleSearch(m.data, m.sender));
+  onMessage('Open', m => handleOpen(m.data, m.sender));
   onMessage('Download', handleDownload);
 });
 
@@ -15,20 +15,26 @@ const nextTabPosition = async () => {
   return tab.index;
 };
 
-const handleSearch = async (ctx: Context) => {
+const handleSearch = async (ctx: Context, sender: Browser.runtime.MessageSender) => {
   const text = ctx.selectedText || ctx.dropText;
   if (!text) return false;
   browser.tabs.create({
     active: false,
     index: await nextTabPosition() + 1,
+    openerTabId: sender.tab?.id,
     url: `https://google.com/search?q=${encodeURIComponent(text)}`,
   });
   return true;
 }
 
-const handleOpen = async (ctx: Context) => {
+const handleOpen = async (ctx: Context, sender: Browser.runtime.MessageSender) => {
   if (!ctx.link) return false;
-  browser.tabs.create({ url: ctx.link, active: false, index: await nextTabPosition() + 1 });
+  browser.tabs.create({
+    url: ctx.link,
+    active: false,
+    openerTabId: sender.tab?.id,
+    index: await nextTabPosition() + 1,
+  });
   return true;
 }
 
